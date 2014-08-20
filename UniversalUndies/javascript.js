03a7c055c2b92193b93cf2e55e051038a2d02d3c
Vincent Lee <lee.m.vincent@gmail.com>
2014-08-20 00:05:30 -0700

    DRY-er code. Still need work on hover. Add jQuery for practice

--- UniversalUndies/javascript.js
+++ UniversalUndies/javascript.js
@@ -113,29 +113,36 @@ function checkoutFunction() {
     alert("Select a size, color, and quantity.");
   }
 }
-
 // Sets selectedColor to pink/mint/army/dotted/beach/surf
-document.getElementById("pink-button").addEventListener('click', selectColorPink,false);
-document.getElementById("mint-button").addEventListener("click", selectColorMint,false);
-document.getElementById("army-button").addEventListener('click', selectColorArmy,false);
-document.getElementById("dotted-button").addEventListener('click', selectColorDotted,false);
-document.getElementById("beach-button").addEventListener('click', selectColorBeach,false);
-document.getElementById("surf-button").addEventListener('click', selectColorSurf,false);
+$(function() {
+  $("#" + colors[0] + "-button").on("click", function() {selectColor(colors[0]);});
+  $("#" + colors[1] + "-button").on("click", function() {selectColor(colors[1]);});
+  $("#" + colors[2] + "-button").on("click", function() {selectColor(colors[2]);});
+  $("#" + colors[3] + "-button").on("click", function() {selectColor(colors[3]);});
+  $("#" + colors[4] + "-button").on("click", function() {selectColor(colors[4]);});
+  $("#" + colors[5] + "-button").on("click", function() {selectColor(colors[5]);});
+});
 
 // Sets selectedSize to s/m/l when clicked
-document.getElementById("size-s").addEventListener('click', selectSizeS,false);
-document.getElementById("size-m").addEventListener('click', selectSizeM,false);
-document.getElementById("size-l").addEventListener('click', selectSizeL,false);
+$(function() {
+  $("#size-" + sizes[0]).on("click", function() {selectSize(sizes[0]);});
+  $("#size-" + sizes[1]).on("click", function() {selectSize(sizes[1]);});
+  $("#size-" + sizes[2]).on("click", function() {selectSize(sizes[2]);});
+});
 
 // Displays highlighted s/m/l icon on hover
-document.getElementById("size-s").addEventListener('mouseover', hoverOnSizeS,false);
-document.getElementById("size-m").addEventListener('mouseover', hoverOnSizeM,false);
-document.getElementById("size-l").addEventListener('mouseover', hoverOnSizeL,false);
+$(function() {
+  $("#size-" + sizes[0]).on("mouseover", function() {hoverOnSize(sizes[0]);});
+  $("#size-" + sizes[1]).on("mouseover", function() {hoverOnSize(sizes[1]);});
+  $("#size-" + sizes[2]).on("mouseover", function() {hoverOnSize(sizes[2]);});
+});
 
 // Reverts to unhighlighted s/m/l icon when hover lost
-document.getElementById("size-s").addEventListener('mouseout', hoverOutSizeS,false);
-document.getElementById("size-m").addEventListener('mouseout', hoverOutSizeM,false);
-document.getElementById("size-l").addEventListener('mouseout', hoverOutSizeL,false);
+$(function() {
+  $("#size-" + sizes[0]).on("mouseout", function() {hoverOffSize(sizes[0]);});
+  $("#size-" + sizes[1]).on("mouseout", function() {hoverOffSize(sizes[1]);});
+  $("#size-" + sizes[2]).on("mouseout", function() {hoverOffSize(sizes[2]);});
+});
 
 document.getElementById("add-cart").addEventListener('click', changeAddToCartTextADDED, false);
 document.getElementById("add-cart").addEventListener('mouseout', changeAddToCartTextREVERT, false);
@@ -144,272 +151,129 @@ document.getElementById("quantityInput").addEventListener('change',selectQuantit
 
 document.getElementById("checkout").addEventListener('click', checkoutFunction, false);
 
-var mainImageChange = document.getElementById("main-image");
 
 function selectQuantity() {
   selectedQuantity = parseInt(document.getElementById("quantityInput").value);
 }
-function selectColorPink() {
-  selectedColor = "pink";
+function changeImage(imagePath, locationID) {
+  document.getElementById(locationID).src = imagePath;
 }
-function selectColorMint() {
-  selectedColor = "mint";
-}
-function selectColorArmy() {
-  selectedColor = "army";
-}
-function selectColorDotted() {
-  selectedColor = "dotted";
-}
-function selectColorBeach() {
-  selectedColor = "beach";
-}
-function selectColorSurf() {
-  selectedColor = "surf";
-}
-
-function selectColorPink(){
-  if(selectedColor != "pink"){
-    selectedColor = "pink";
-    changeImageToClickedPink();
-    changeImageToUnclickedMint();
-    changeImageToUnclickedArmy();
-    changeImageToUnclickedDotted();
-    changeImageToUnclickedBeach();
-    changeImageToUnclickedSurf();
-    mainImageChange.src = "pictures/pink.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedPink();
-  }
-};
-
-function selectColorMint(){
-  if(selectedColor != "mint"){
-    selectedColor = "mint";
-    changeImageToClickedMint();
-    changeImageToUnclickedPink();
-    changeImageToUnclickedArmy();
-    changeImageToUnclickedDotted();
-    changeImageToUnclickedBeach();
-    changeImageToUnclickedSurf();
-    mainImageChange.src = "pictures/mint.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedMint();
-  }
-};
-
-function selectColorArmy(){
-  if(selectedColor != "army"){
-    selectedColor = "army";
-    changeImageToClickedArmy();
-    changeImageToUnclickedPink();
-    changeImageToUnclickedMint();
-    changeImageToUnclickedDotted();
-    changeImageToUnclickedBeach();
-    changeImageToUnclickedSurf();
-    mainImageChange.src = "pictures/army.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedArmy();
-  }
-};
-
-function selectColorDotted(){
-  if(selectedColor != "dotted"){
-    selectedColor = "dotted";
-    changeImageToClickedDotted();
-    changeImageToUnclickedPink();
-    changeImageToUnclickedMint();
-    changeImageToUnclickedArmy();
-    changeImageToUnclickedBeach();
-    changeImageToUnclickedSurf();
-    mainImageChange.src = "pictures/dotted.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedDotted();
-  }
-};
-
-function selectColorBeach(){
-  if(selectedColor != "beach"){
-    selectedColor = "beach";
-    changeImageToClickedBeach();
-    changeImageToUnclickedPink();
-    changeImageToUnclickedMint();
-    changeImageToUnclickedArmy();
-    changeImageToUnclickedDotted();
-    changeImageToUnclickedSurf();
-    mainImageChange.src = "pictures/beach.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedBeach();
-  }
-};
-
-function selectColorSurf(){
-  if(selectedColor != "surf"){
-    selectedColor = "surf";
-    changeImageToClickedSurf();
-    changeImageToUnclickedPink();
-    changeImageToUnclickedMint();
-    changeImageToUnclickedArmy();
-    changeImageToUnclickedDotted();
-    changeImageToUnclickedBeach();
-    mainImageChange.src = "pictures/surf.jpg";
-  }else {
-    selectedColor = "void";
-    changeImageToUnclickedSurf();
-  }
-};
-
-function changeImageToClickedPink() {
-  document.getElementById("pink-button").src = "pictures/pink-button-click.jpg";
-}
-
-function changeImageToClickedMint() {
-  document.getElementById("mint-button").src = "pictures/mint-button-click.jpg";
-}
-
-function changeImageToClickedArmy() {
-  document.getElementById("army-button").src = "pictures/army-button-click.jpg";
-}
-
-function changeImageToClickedDotted() {
-  document.getElementById("dotted-button").src = "pictures/dotted-button-click.jpg";
-}
-
-function changeImageToClickedBeach() {
-  document.getElementById("beach-button").src = "pictures/beach-button-click.jpg";
-}
-
-function changeImageToClickedSurf() {
-  document.getElementById("surf-button").src = "pictures/surf-button-click.jpg";
-}
-
-//Unclicked Color Buttons
-
-function changeImageToUnclickedPink() {
-  document.getElementById("pink-button").src = "pictures/pink-button.jpg";
-}
-
-function changeImageToUnclickedMint() {
-  document.getElementById("mint-button").src = "pictures/mint-button.jpg";
-}
-
-function changeImageToUnclickedArmy() {
-  document.getElementById("army-button").src = "pictures/army-button.jpg";
-}
-
-function changeImageToUnclickedDotted() {
-  document.getElementById("dotted-button").src = "pictures/dotted-button.jpg";
-}
-
-function changeImageToUnclickedBeach() {
-  document.getElementById("beach-button").src = "pictures/beach-button.jpg";
-}
-
-function changeImageToUnclickedSurf() {
-  document.getElementById("surf-button").src = "pictures/surf-button.jpg";
-}
-
-
-
-function selectSizeS() {
-  if(selectedSize != "s" ) {
-    selectedSize = "s";
-    changeImageToClickedS();
-    changeImageToUnclickedM();
-    changeImageToUnclickedL();
-  }
-  else {
-    selectedSize = "void";
-    changeImageToUnclickedS();
+function selectSize(size) {
+  selectedSize = size;
+  switch(size) {
+    case sizes[0]:
+      changeImage("pictures/size-" + sizes[0] + "-click.png", "size-" + sizes[0]);
+      changeImage("pictures/size-" + sizes[1] + ".png", "size-" + sizes[1]);
+      changeImage("pictures/size-" + sizes[2] + ".png", "size-" + sizes[2]);
+      break;
+    case sizes[1]:
+      changeImage("pictures/size-" + sizes[0] + ".png", "size-" + sizes[0]);
+      changeImage("pictures/size-" + sizes[1] + "-click.png", "size-" + sizes[1]);
+      changeImage("pictures/size-" + sizes[2] + ".png", "size-" + sizes[2]);
+      break;
+    case sizes[2]:
+      changeImage("pictures/size-" + sizes[0] + ".png", "size-" + sizes[0]);
+      changeImage("pictures/size-" + sizes[1] + ".png", "size-" + sizes[1]);
+      changeImage("pictures/size-" + sizes[2] + "-click.png", "size-" + sizes[2]);
+      break;
   }
 }
-function selectSizeM() {
-  if(selectedSize != "m") {
-    selectedSize = "m";
-    changeImageToClickedM();
-    changeImageToUnclickedS();
-    changeImageToUnclickedL();
-  }
-  else {
-    selectedSize = "void";
-    changeImageToUnclickedM();
-  }
-}
-function selectSizeL() {
-  if(selectedSize != "l") {
-    selectedSize = "l";
-    changeImageToClickedL();
-    changeImageToUnclickedS();
-    changeImageToUnclickedM();
-  }
-  else {
-    selectedSize = "void";
-    changeImageToUnclickedL();
-  }
-}
-function hoverOnSizeS() {
-  if(selectedSize != "s") {
-    changeImageToHoverS();
-  }
-}
-function hoverOnSizeM() {
-  if(selectedSize != "m") {
-    changeImageToHoverM();
-  }
-}
-function hoverOnSizeL() {
-  if(selectedSize != "l") {
-    changeImageToHoverL();
-  }
-}
-function hoverOutSizeS() {
-  if(selectedSize != "s") {
-    changeImageToUnclickedS();
-  }
-}
-function hoverOutSizeM() {
-  if(selectedSize != "m") {
-    changeImageToUnclickedM();
-  }
-}
-function hoverOutSizeL() {
-  if(selectedSize != "l") {
-    changeImageToUnclickedL();
+// function hoverOnSize(size) {
+//   if(selectedSize != size) {
+//     switch(size) {
+//       case sizes[0]:
+//         changeImage("pictures/size-" + sizes[0] + "-hover.png", "size-" + sizes[0]);
+//         changeImage("pictures/size-" + sizes[1] + ".png", "size-" + sizes[1]);
+//         changeImage("pictures/size-" + sizes[2] + ".png", "size-" + sizes[2]);
+//         break;
+//       case sizes[1]:
+//         changeImage("pictures/size-" + sizes[0] + ".png", "size-" + sizes[0]);
+//         changeImage("pictures/size-" + sizes[1] + "-hover.png", "size-" + sizes[1]);
+//         changeImage("pictures/size-" + sizes[2] + ".png", "size-" + sizes[2]);
+//         break;
+//       case sizes[2]:
+//         changeImage("pictures/size-" + sizes[0] + ".png", "size-" + sizes[0]);
+//         changeImage("pictures/size-" + sizes[1] + ".png", "size-" + sizes[1]);
+//         changeImage("pictures/size-" + sizes[2] + "-hover.png", "size-" + sizes[2]);
+//         break;
+//     }
+//   }
+// }
+// function hoverOffSize(size) {
+//   if(selectedSize != size) {
+//     switch(size) {
+//       case sizes[0]:
+//         changeImage("pictures/size-" + sizes[0] + ".png", "size-" + sizes[0]);
+//         break;
+//       case sizes[1]:
+//         changeImage("pictures/size-" + sizes[1] + ".png", "size-" + sizes[1]);
+//         break;
+//       case sizes[2]:
+//         changeImage("pictures/size-" + sizes[2] + ".png", "size-" + sizes[2]);
+//         break;
+//     }
+//   }
+// }
+function selectColor(color) {
+  selectedColor = color
+  switch(color) {
+    case colors[0]:
+      changeImage("pictures/" + colors[0] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button-click.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button.jpg", colors[5] + "-button");
+      break;
+    case colors[1]:
+      changeImage("pictures/" + colors[1] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button-click.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button.jpg", colors[5] + "-button");
+      break;
+    case colors[2]:
+      changeImage("pictures/" + colors[2] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button-click.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button.jpg", colors[5] + "-button");
+      break;
+    case colors[3]:
+      changeImage("pictures/" + colors[3] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button-click.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button.jpg", colors[5] + "-button");
+      break;
+    case colors[4]:
+      changeImage("pictures/" + colors[4] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button-click.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button.jpg", colors[5] + "-button");
+      break;
+    case colors[5]:
+      changeImage("pictures/" + colors[5] + ".jpg","main-image");
+      changeImage("pictures/" + colors[0] + "-button.jpg", colors[0] + "-button");
+      changeImage("pictures/" + colors[1] + "-button.jpg", colors[1] + "-button");
+      changeImage("pictures/" + colors[2] + "-button.jpg", colors[2] + "-button");
+      changeImage("pictures/" + colors[3] + "-button.jpg", colors[3] + "-button");
+      changeImage("pictures/" + colors[4] + "-button.jpg", colors[4] + "-button");
+      changeImage("pictures/" + colors[5] + "-button-click.jpg", colors[5] + "-button");
+      break;
   }
 }
 
-function changeImageToClickedS() {
-  document.getElementById("size-s").src = "pictures/size-s-click.png";
-}
-function changeImageToUnclickedS() {
-  document.getElementById("size-s").src = "pictures/size-s.png";
-}
-function changeImageToHoverS() {
-  document.getElementById("size-s").src = "pictures/size-s-hover.png";
-}
-function changeImageToClickedM() {
-  document.getElementById("size-m").src = "pictures/size-m-click.png";
-}
-function changeImageToUnclickedM() {
-  document.getElementById("size-m").src = "pictures/size-m.png";
-}
-function changeImageToHoverM() {
-  document.getElementById("size-m").src = "pictures/size-m-hover.png";
-}
-function changeImageToClickedL() {
-  document.getElementById("size-l").src = "pictures/size-l-click.png";
-}
-function changeImageToUnclickedL() {
-  document.getElementById("size-l").src = "pictures/size-l.png";
-}
-function changeImageToHoverL() {
-  document.getElementById("size-l").src = "pictures/size-l-hover.png";
-}
 function changeAddToCartTextADDED(){
   document.getElementById("add-cart").value = "ITEM ADDED";
 }
@@ -417,7 +281,9 @@ function changeAddToCartTextREVERT(){
   document.getElementById("add-cart").value = "ADD TO CART";
 }
 
-
-
-//---- The only actual running code that runs on load.
+function changeImageClickedSize(size, attr) {
+  // attr is either "click" or "hover".
+  document.getElementById("size-" + size).src = "pictures/size-" + size + "-" + attr+ ".png";
+}
+// ---- The only actual running code that runs on load.
 makeProductList();
